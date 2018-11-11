42aed246c5af29da215ca5f40bc7491ae4f58fe0
unknown <christopher@seander.se>
2018-11-11 01:34:56 +0100

    Updates

---
+++ precache-manifest.236991e16de89a99f219325f51e77dd7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a24bb57656f0703cce63",
+    "url": "/static/js/main.a24bb576.chunk.js"
+  },
+  {
+    "revision": "1c7720c85de3a20918d6",
+    "url": "/static/js/1.1c7720c8.chunk.js"
+  },
+  {
+    "revision": "a24bb57656f0703cce63",
+    "url": "/static/css/main.1beaba5b.chunk.css"
+  },
+  {
+    "revision": "d80b881ec0f8bb8687b1dff6beafd704",
+    "url": "/index.html"
+  }
+];
